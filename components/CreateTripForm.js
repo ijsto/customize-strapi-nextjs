3d3902cc621b317414298453e2dd8de5f70e31c9
ScottAgirs <scott@ijs.to>
2021-03-18 18:34:12 -0400

    feat: Add covid checkbox to form
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- components/CreateTripForm.js
+++ components/CreateTripForm.js
@@ -3,6 +3,7 @@ import FIRST_MT from '../api/mutations/FIRST_MT';
 import FIRST_QY from '../api/queries/FIRST_QY';
 import Button from './common/Button';
 import useInput from '../hooks/useInput';
+import Checkbox from './dataEntry/Checkbox';
 
 const CreateTripForm = () => {
   const {
@@ -10,6 +11,11 @@ const CreateTripForm = () => {
     bind: bindTripTitle,
     reset: resetTripTitle,
   } = useInput('');
+  const {
+    checked: covidCheck,
+    bind: bindCovidCheck,
+    reset: resetCovidCheck,
+  } = useInput(false);
 
   const [createTrip, { error }] = useMutation(FIRST_MT, {
     onError(err) {
@@ -18,35 +24,38 @@ const CreateTripForm = () => {
     },
   });
 
-  if (error)
-    return (
-      <p>
-        Error:
-        <pre>{JSON.stringify(error, null, 2)}</pre>
-      </p>
-    );
-
   const handleCreateTrip = async e => {
     e.preventDefault();
     await createTrip({
       refetchQueries: [{ query: FIRST_QY }],
       variables: {
         destination: `Scranton, PA`,
-        isTravellerCovidFree: true,
+        isTravellerCovidFree: covidCheck || false,
         title: tripTitle,
       },
     });
     resetTripTitle();
+    resetCovidCheck(false);
   };
   return (
-    <form onSubmit={handleCreateTrip}>
-      <div style={{ display: 'flex' }}>
-        <input type="text" {...bindTripTitle} />
-        <Button style={{ minWidth: 200 }} type="submit">
-          Create Trip
-        </Button>
-      </div>
-    </form>
+    <>
+      {error && (
+        <h3 style={{ color: '#cd0200', textAlign: 'center' }}>
+          Uh-oh! {error.message}
+        </h3>
+      )}
+      <form onSubmit={handleCreateTrip}>
+        <div style={{ display: 'flex' }}>
+          <input type="text" {...bindTripTitle} />
+          <Button style={{ minWidth: 200 }} type="submit">
+            Create Trip
+          </Button>
+        </div>
+        <div>
+          <Checkbox label="Covid test was negative" {...bindCovidCheck} />
+        </div>
+      </form>
+    </>
   );
 };
 
