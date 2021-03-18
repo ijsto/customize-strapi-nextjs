33021b20ed7f4f4cc7551f789c5bc4a819078f35
ScottAgirs <scott@ijs.to>
2021-03-18 18:32:42 -0400

    feat: Add checkbox handling to useInput()
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- hooks/useInput.js
+++ hooks/useInput.js
@@ -2,15 +2,23 @@ import { useState } from 'react';
 
 const useInput = initialValue => {
   const [value, setValue] = useState(initialValue);
+  const [checked, setChecked] = useState(initialValue);
 
   return {
     bind: {
+      checked,
       onChange: event => {
-        setValue(event.target.value);
+        if (typeof value === 'boolean') {
+          return setChecked(event.target.checked);
+        }
+        return setValue(event.target.value);
       },
       value,
     },
-    reset: () => setValue(''),
+    checked,
+    reset: resetValue => {
+      return typeof resetValue === 'boolean' ? setChecked(false) : setValue('');
+    },
     setValue,
     value,
   };
