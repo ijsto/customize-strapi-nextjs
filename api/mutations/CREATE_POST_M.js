bda316c74517f2b446bd7c0fe22b6450b6536b62
ScottAgirs <scott@ijs.to>
2021-04-18 17:54:44 -0400

    refactor: Make $body optional
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/mutations/CREATE_POST_M.js
+++ api/mutations/CREATE_POST_M.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 const CREATE_POST_M = gql`
-  mutation CREATE_POST_M($title: String!, $body: String!) {
+  mutation CREATE_POST_M($title: String!, $body: String) {
     createPost(input: { data: { title: $title, body: $body } }) {
       post {
         id
