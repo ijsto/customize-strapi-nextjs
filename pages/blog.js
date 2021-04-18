d7e2014166e435813d76b5e31e16eea6faccf4b5
ScottAgirs <scott@ijs.to>
2021-04-18 17:54:51 -0400

    feat(Blog): Init page
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ pages/blog.js
@@ -0,0 +1,44 @@
+import Cookies from 'js-cookie';
+import { useQuery } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
+import POSTS_Q from '../api/queries/POSTS_Q';
+
+import Authentication from '../components/dataEntry/Authentication';
+import CreatePostForm from '../components/CreatePostForm';
+import PostList from '../components/PostList';
+
+const BlogPage = () => {
+  const [token, setToken] = useState(null);
+
+  const { data, error } = useQuery(POSTS_Q, {
+    onError(err) {
+      // eslint-disable-next-line no-console
+      console.log('onError :: err', err);
+    },
+  });
+
+  useEffect(() => {
+    setToken(Cookies.get('jwt'));
+  }, []);
+
+  if (error) return <p>Error: {JSON.stringify(error)}</p>;
+  const posts = data?.posts ?? [];
+
+  return (
+    <div style={{ margin: '0 auto', maxWidth: 600 }}>
+      <h1 style={{ textAlign: 'center' }}>Donald's qwaavel bwog!</h1>
+
+      {!token && <Authentication />}
+
+      <CreatePostForm />
+
+      {posts?.length > 0 && (
+        <h2 style={{ textAlign: 'center' }}>Existing posts</h2>
+      )}
+      <PostList posts={posts} />
+    </div>
+  );
+};
+
+export default BlogPage;
